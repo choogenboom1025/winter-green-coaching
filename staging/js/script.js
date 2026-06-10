52226d13db6b2f3c704a82917e8f57f2c57dbe27
choogenboom1025 <64430909+choogenboom1025@users.noreply.github.com>
2026-06-10 12:28:00 -0700

    Create staging JavaScript file for preview testing

---
+++ staging/js/script.js
@@ -0,0 +1,4 @@
+// Staging version of script - For preview testing only
+// Copy changes here first, preview them, then copy to main script.js when ready
+
+console.log("Staging version loaded - This is for preview testing only");
